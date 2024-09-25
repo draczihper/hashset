38f3206d98018f1e03315bd029a8705e8a1fdb8a
Drac Zihper <draczihper@gmail.com>
2024-09-25 20:12:58 +0300

    Add a hashing function

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ class HashSet {
         this.loadFactor = 0.75;
         this.size = 0;
     }
+
+    hash(key) {
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for (let i = 0; i < key.length; i++) {
+            hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
+        }
+    }
 }
